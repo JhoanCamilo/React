0cbebade36a3b26dff80a0778411eb5983268c2f
Jhoan Camilo Arango Monsalve <51723353+JhoanCamilo@users.noreply.github.com>
2023-05-05 20:36:50 -0500

    Add files via upload

---
+++ useContext/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import {SearchBar} from "./SearchBar/SearchBar.jsx"
+function App() {
+  return (
+    <>
+      <h1>UseContext</h1>
+      <p>Learning about useContext with a simple search bar</p>
+      <SearchBar/>
+    </>
+  )
+}
+
+export default App
